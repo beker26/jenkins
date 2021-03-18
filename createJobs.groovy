93cc643cb765204d2ef3af6c09f252960d8eaed0
rodrigo.costa <beker_la@hotmail.com>
2021-03-18 12:11:10 -0300

    correção no app

--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,17 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/beker26/passeio.git'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
+}
